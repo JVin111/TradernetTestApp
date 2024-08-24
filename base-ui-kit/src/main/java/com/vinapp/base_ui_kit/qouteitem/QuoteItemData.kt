6ab1e87451ebc0b0260e0f3879ac50ea318b5262
JVin_111 <j_vin111@bk.ru>
2024-08-24 17:46:40 +0300

    Added highlighting for percentage and additional padding for QuouteItem

--- base-ui-kit/src/main/java/com/vinapp/base_ui_kit/qouteitem/QuoteItemData.kt
+++ base-ui-kit/src/main/java/com/vinapp/base_ui_kit/qouteitem/QuoteItemData.kt
@@ -7,7 +7,7 @@ data class QuoteItemData(
     val tickerIcon: String?,
     val tickerTitle: String,
     val subtitle: String,
-    val percentageChange: String,
+    val percentageChange: String?,
     val isGrowing: Boolean,
     val lastTradePrice: String,
     val priceChange: String
@@ -16,11 +16,11 @@ data class QuoteItemData(
         tickerIcon = null,
         tickerTitle = quote.ticker,
         subtitle = if (!quote.exchangeName.isNullOrBlank() && !quote.securityName.isNullOrBlank()) "${quote.exchangeName} | ${quote.securityName}" else "-",
-        percentageChange = buildString {
-            quote.percentageChange?.let {
+        percentageChange = quote.percentageChange?.let {
+            buildString {
                 if (it > 0.0F) append("+")
                 append(String.format(Locale.US, "%.2f%%", it))
-            } ?: append("0.0%")
+            }
         },
         isGrowing = quote.percentageChange?.let { it >= 0.0F } ?: true,
         lastTradePrice = quote.lastTradePrice?.toBigDecimal()?.toString() ?: "-",
